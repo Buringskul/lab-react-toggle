d9f5760150bb8aec521153b51a86c519a8f02b48
Buringskul <simonaisakova6@gmail.com>
2024-09-24 13:31:54 -0400

    finished lab

---
+++ src/components/Board.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Board(props) {
+  let className = "board";
+  if (props.selected) {
+    className += " selected";
+  }
+
+  return <div className={className}>{props.index + 1}</div>;
+}
+
+export default Board;
--- src/components/BoardSwitcher.jsx
+++ src/components/BoardSwitcher.jsx
@@ -1,25 +1,21 @@
-import React from "react";
-
-function Board(props) {
-  let className = "board";
-  if (props.selected) {
-    className += " selected";
-  }
-
-  return <div className={className}>{props.index + 1}</div>;
-}
+import React, { useState } from "react";
+import Board from "./Board";
 
 function BoardSwitcher(props) {
-  let boards = [];
-  for (let ii = 0; ii < props.numBoards; ii++) {
-    let isSelected = ii === 0;
-    boards.push(<Board index={ii} selected={isSelected} key={ii} />);
+  const [currentBoard, setCurrentBoard] = useState(0);
+  const boards = Array.from(
+      { length: props.numBoards },
+      (_, i) => <Board key={i} index={i} selected={i === currentBoard} />,
+  );
+
+  function handleButton() {
+    setCurrentBoard((currentBoard+1) % props.numBoards)
   }
 
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button>Toggle</button>
+      <button onClick={handleButton}>Toggle</button>
     </div>
   );
 }
